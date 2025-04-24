6904e66fdf90afb176c3cbc021053aa50dbd09b2
Mohammad-Irshad <132482951+Mohammad-Irshad@users.noreply.github.com>
2025-04-24 23:20:11 +0530

    Update Posts.jsx

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -205,9 +205,12 @@ const Posts = ({
                     <FaComment style={{ cursor: "pointer", color: "red" }} />
                   </Link>
                 ) : (
-                  <Link to={`/post/${post._id}`}>
-                    <FaComment style={{ cursor: "pointer", color: "black" }} />{" "}
-                  </Link>
+                  <div>
+                    <Link to={`/post/${post._id}`}>
+                      <FaComment style={{ cursor: "pointer", color: "black" }} />{" "}
+                    </Link>
+                    <span>{post.postComments}</span>
+                  </div>
                 )}
                 {/* <FaShare style={{cursor : 'pointer'}}/> */}
                 <FaBookmark
